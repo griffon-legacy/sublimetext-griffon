bcc69f3d72a2bbc367e75e564b1aac17076705f2
aalmiray <aalmiray@gmail.com>
2012-11-21 09:56:39 +0100

    Detect .gdoc files as Texttile. Based on patch provided by @nobeans to sublimetext-grails

--- SyntaxDetector.py
+++ SyntaxDetector.py
@@ -31,6 +31,10 @@ class GriffonSyntaxCommand(sublime_plugin.EventListener):
 
         self.set_file_variables()
 
+        if self.ext == '.gdoc':
+            self.set_syntax('Textile', 'Textile')
+            return
+
         if not self.ext == '.groovy':
             return
 
